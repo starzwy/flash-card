68d93079d450e492339ae2b4ac387b6c8f0ca345
starzwy <162142976+starzwy@users.noreply.github.com>
2025-06-23 12:23:26 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,17 @@
+criaCartao(
+    'Programação',
+    'O que é Javascript?',
+    'O javascript é uma linguagem de programação'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual é a capital do Paraná?',
+    'A capital do Paraná é Curitiba'
+)
+
+criaCartao(
+    'Programação',
+    'O que é Python?',
+    'O Python é uma linguagem de programação'
+)
